66fd880dd8627d2cca1c8d8f476e8eb03249d5c0
GAAROS <82255555+GAAROS@users.noreply.github.com>
2021-06-22 13:38:09 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,20 @@
+import requests
+
+from datetime import datetime
+api_key='c63eb07784e4ba385b91ef3ecea0f226'
+location=input('enter your city name ')
+
+compl_api_link="http://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link=requests.get(compl_api_link)
+api_data=api_link.json()
+
+temp_city=((api_data['main']['temp'])-273.15)
+weather_desc=api_data['weather'][0]['description']
+hmdt=api_data['main']['humidity']
+wind_spd=api_data['wind']['speed']
+
+print("weather stats for  ",location.upper())
+print("current temperature : ",temp_city)
+print("current weather description : ",weather_desc)
+print("current humidity : ",hmdt,'%')
+print("current wind speed : ",wind_spd,'kmph')
